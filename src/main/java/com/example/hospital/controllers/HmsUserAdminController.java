39d76a7f0e1a3ccba4e27244d102e76376ca38e0
Thiaka Brian Muriithi <thiakabrian@gmail.com>
2026-04-02 00:53:22 +0300

    Unused import

--- src/main/java/com/example/hospital/controllers/HmsUserAdminController.java
+++ src/main/java/com/example/hospital/controllers/HmsUserAdminController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/users")
